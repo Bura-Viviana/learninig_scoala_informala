071f758a914ada1c1db7ac6875443004218044ac
Bura Viviana <viviana_mariam@yahoo.com>
2021-01-25 09:57:55 +0200

    My first project commit.

--- learning.py
+++ learning.py
@@ -1,7 +1,10 @@
 nums = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
-increasing_nums = nums[5], nums[3], nums[4], nums[6], nums[8], nums[9], nums[0], nums[1], nums[2]
-decreasing_nums = nums[-8], nums[-9], nums[-10], nums[-1], nums[-2], nums[-4], nums[-6], nums[-7], nums[-5]
+increasing_nums = list(nums)
+print(nums)
+increasing_nums.sort()
 print(increasing_nums)
+decreasing_nums = list(increasing_nums)
+decreasing_nums.reverse()
 print(decreasing_nums)
 print(increasing_nums[1::2])
 print(increasing_nums[::2])
